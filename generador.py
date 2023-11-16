686842d8e3efa0f33c463ad9097a8ad90a08e249
AlejandroSalas1143 <72144337+AlejandroSalas1143@users.noreply.github.com>
2023-11-15 23:04:06 -0500

    Add files via upload
    Generador actualizado

--- generador.py
+++ generador.py
@@ -10,6 +10,8 @@ from collections import defaultdict
 from itertools import combinations
 import glob
 from pathlib import Path
+from datetime import datetime
+import shutil
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Descripción de tu script.")
@@ -17,18 +19,56 @@ def parse_args():
     parser.add_argument("--fi", "--fecha_inicial", help="Fecha inicial en formato dd-mm-aa")
     parser.add_argument("--ff", "--fecha_final", help="Fecha final en formato dd-mm-aa")
     parser.add_argument("-ht", "--hashtags_file", help="Nombre de archivo de texto con hashtags")
+    
+    # New options for graph and JSON generation
+    parser.add_argument("-grt", "--generate_retweet_graph", action="store_true", help="Generar grafo de retweets")
+    parser.add_argument("-jrt", "--generate_retweet_json", action="store_true", help="Generar JSON de retweets")
+    parser.add_argument("-gm", "--generate_mentions_graph", action="store_true", help="Generar grafo de menciones")
+    parser.add_argument("-jm", "--generate_mentions_json", action="store_true", help="Generar JSON de menciones")
+    parser.add_argument("-gcrt", "--generate_corretweet_graph", action="store_true", help="Generar grafo de corretweets")
+    parser.add_argument("-jcrt", "--generate_corretweet_json", action="store_true", help="Generar JSON de corretweets")
+
     args = parser.parse_args()
-    return args.directory, args.fi, args.ff, args.hashtags_file
+    return args.directory, args.fi, args.ff, args.hashtags_file, args
 
 def get_tweet_id(tweet):
     return tweet['id_str'] if 'retweeted_status' in tweet else str(tweet['id'])
 
-def process_original_tweet(tweet, retweets_info, mentions_info, hashtags_set=None):
+def validate_tweet_date(tweet, fi, ff):
+    if 'created_at' in tweet:
+        tweet_date_str = tweet['created_at']
+        
+        # Convertir la fecha del tweet al formato deseado
+        tweet_date = datetime.strptime(tweet_date_str, "%a %b %d %H:%M:%S +0000 %Y").date()
+            
+        if fi is not None:
+            # Convertir la fecha inicial al formato deseado
+            fi = datetime.strptime(fi, "%d-%m-%y").date()
+            #print("soy fi: ",fi)
+            if tweet_date < fi:
+                return False
+
+        if ff is not None:
+            # Convertir la fecha final al formato deseado
+            ff = datetime.strptime(ff, "%d-%m-%y").date()
+            if tweet_date > ff:
+                return False
+
+        # Si no se generó ninguna excepción, la fecha se validó correctamente
+        return True
+
+    return True
+
+def process_original_tweet(tweet, retweets_info, mentions_info, hashtags_set=None, fi=None, ff=None):
+    if fi is not None or ff is not None:
+        if not validate_tweet_date(tweet, fi, ff):
+            # Validation failed, do something or simply return
+            return
+
     if 'user' in tweet:
         tweet_author_username = tweet['user']['screen_name']
         tweet_id = get_tweet_id(tweet)
 
-        # Verificar si hay hashtags y filtrar por ellos
         if 'entities' in tweet and 'hashtags' in tweet['entities']:
             tweet_hashtags = {tag['text'].lower() for tag in tweet['entities']['hashtags']}
             if hashtags_set and not tweet_hashtags.intersection(hashtags_set):
@@ -39,12 +79,15 @@ def process_original_tweet(tweet, retweets_info, mentions_info, hashtags_set=Non
 
         process_mentions(tweet, mentions_info)
 
-def process_retweet(tweet, retweets_info, mentions_info, hashtags_set=None):
+def process_retweet(tweet, retweets_info, mentions_info, hashtags_set=None, fi=None, ff=None):
+    if fi is not None or ff is not None:
+        if not validate_tweet_date(tweet, fi, ff):
+            return
+
     if 'retweeted_status' in tweet and 'user' in tweet['retweeted_status']:
         retweet_author_username = tweet['retweeted_status']['user']['screen_name']
         retweeted_tweet_id = get_tweet_id(tweet['retweeted_status'])
 
-        # Verificar si hay hashtags y filtrar por ellos
         if 'entities' in tweet['retweeted_status'] and 'hashtags' in tweet['retweeted_status']['entities']:
             retweet_hashtags = {tag['text'].lower() for tag in tweet['retweeted_status']['entities']['hashtags']}
             if hashtags_set and not retweet_hashtags.intersection(hashtags_set):
@@ -64,91 +107,51 @@ def process_mentions(tweet, mentions_info):
             mentions_info.setdefault(mentioned_username, {"mentions": []})
             mentions_info[mentioned_username]["mentions"].append({"mentionBy": tweet['user']['screen_name'], "tweets": [get_tweet_id(tweet)]})
 
-def decompress_and_create_json_files(directory, fecha_inicial=None, fecha_final=None, hashtags_file=None):
+def decompress_and_create_json_files(directory, hashtags_file=None, fi=None, ff=None):
     retweets_info = {}
     mentions_info = {}
-    json_files = [] 
+    detener_procesamiento_ff = False
+
     # Cargar hashtags desde el archivo
     hashtags_set = set()
     if hashtags_file:
         with open(hashtags_file, 'r') as hashtags_file:
             hashtags_set = {line.strip().lower() for line in hashtags_file}
-    #FILE_PATHH =[]
+    
     # Utilizamos Path para manejar rutas de manera más eficiente
     base_path = Path(directory)
 
-    if fecha_inicial!=None:
-        fecha_iniciall = convert_year_to_4_digits(fecha_inicial[-2:])
-        directory_path_i = os.path.join(fecha_iniciall, fecha_inicial[3:5], fecha_inicial[0:2])
-        directory_path_i = directory_path_i.replace("\\", "/")
-    
-    if fecha_final!=None:
-        fecha_finall = convert_year_to_4_digits(fecha_final[-2:])
-        directory_path_f = os.path.join(fecha_finall, fecha_final[3:5], fecha_final[0:2])
-        directory_path_f = directory_path_f.replace("\\", "/")
-
     file_paths = base_path.rglob('*.json.bz2')
 
-    file_paths_list = list(file_paths)
-
-    if fecha_inicial==None and fecha_final==None:
-        file_paths_generator=iter(file_paths)
-    else:
-        if fecha_inicial!=None and fecha_final==None:
-            for i, ruta in enumerate(file_paths_list):
-                A= str(ruta).replace("\\", "/")
-                if directory_path_i in str(A):
-                    # La cadena de texto ha sido encontrada, recortar la lista desde este punto
-                    FILE_PATHH= file_paths_list[i:]
-                    break
-            file_paths_generator = iter(FILE_PATHH)
-        else:
-            if fecha_final!=None and fecha_inicial==None:
-                for i, ruta in reversed(list(enumerate(file_paths_list))):
-                    A= str(ruta).replace("\\", "/")
-                    if directory_path_f in str(A):
-                    # La cadena de texto ha sido encontrada, recortar la lista desde este punto
-                        FILE_PATHHf= file_paths_list[:i]
-                        break
-                file_paths_generator = iter(FILE_PATHHf)
-            else:
-                for i, ruta in enumerate(file_paths_list):
-                        A= str(ruta).replace("\\", "/")
-                        if directory_path_i in str(A):
-                            # La cadena de texto ha sido encontrada, recortar la lista desde este punto
-                            FILE_PATHH= file_paths_list[i:]
-                            break
-                file_paths_generator = iter(FILE_PATHH)
-                for i, ruta in reversed(list(enumerate(FILE_PATHH))):
-                    A= str(ruta).replace("\\", "/")
-                    if directory_path_f in str(A):
-                        # La cadena de texto ha sido encontrada, recortar la lista desde este punto
-                        FILE_PATHHf= FILE_PATHH[:i]
-                        break
-                file_paths_generator = iter(FILE_PATHHf)
-
-    
-    for file_path in file_paths_generator:
+    for file_path in file_paths:
         json_file_path = file_path.with_suffix('.json')
-        json_files.append(json_file_path)
 
         with bz2.BZ2File(file_path, 'rb') as source, open(json_file_path, 'wb') as target:
             target.write(source.read())
+        #print(f"Archivo JSON descomprimido: {json_file_path}")
 
         with open(json_file_path, 'r', encoding='utf-8') as json_file:
             for line in json_file:
                 tweet = json.loads(line)
                 if 'retweeted_status' in tweet:
-                    process_retweet(tweet, retweets_info, mentions_info, hashtags_set)
+                    process_retweet(tweet, retweets_info, mentions_info, hashtags_set, fi, ff)
                 else:
-                    process_original_tweet(tweet, retweets_info, mentions_info, hashtags_set)
+                    process_original_tweet(tweet, retweets_info, mentions_info, hashtags_set, fi, ff)
 
-    return retweets_info, mentions_info, json_files
+                if ff is not None and not validate_tweet_date(tweet, fi, ff):
+                    # La validación falló, establecer la bandera para detener el procesamiento por ff
+                    detener_procesamiento_ff = True
+                
+            #if detener_procesamiento_ff:
+             #   break
+
+
+    return retweets_info, mentions_info
+
 
 def decompress_and_create_json_files_directory(directory, hashtags_file=None):
     retweets_info = {}
     mentions_info = {}
-    json_files = []
     hashtags_set = set()
     if hashtags_file:
         with open(hashtags_file, 'r') as hashtags_file:
@@ -162,7 +165,6 @@ def decompress_and_create_json_files_directory(directory, hashtags_file=None):
 
     for file_path in file_paths:
         json_file_path = file_path.with_suffix('.json')
-        json_files.append(json_file_path)
 
         with bz2.BZ2File(file_path, 'rb') as source, open(json_file_path, 'wb') as target:
             target.write(source.read())
@@ -176,13 +178,13 @@ def decompress_and_create_json_files_directory(directory, hashtags_file=None):
                 else:
                     process_original_tweet(tweet, retweets_info, mentions_info, hashtags_set)
 
-    return retweets_info, mentions_info,json_files
+    return retweets_info, mentions_info
 
 def convert_year_to_4_digits(year):
     if len(year) == 2:
         return "20" + year
 
-def generate_retweets_json(retweets_info):
+def generate_retweets_json(retweets_info, arg):
     retweets_json = {"retweets": []}
 
     for author, author_info in retweets_info.items():
@@ -201,13 +203,13 @@ def generate_retweets_json(retweets_info):
 
     # Ordenar el JSON por número total de retweets al usuario (de mayor a menor)
     retweets_json["retweets"] = sorted(retweets_json["retweets"], key=lambda x: x["receivedRetweets"], reverse=True)
-
-    with open("rt.json", "w", encoding="utf-8") as json_file:
-        json.dump(retweets_json, json_file, ensure_ascii=False, indent=2)
+    if arg==True:
+        with open("rt.json", "w", encoding="utf-8") as json_file:
+            json.dump(retweets_json, json_file, ensure_ascii=False, indent=2)
 
     return retweets_json
 
-def generate_mentions_json(mentions_info):
+def generate_mentions_json(mentions_info, arg):
     mentions_json = {"mentions": []}
     #pprint(mentions_info)
 
@@ -223,9 +225,9 @@ def generate_mentions_json(mentions_info):
 
     # Ordenar el JSON por número total de menciones al usuario (de mayor a menor)
     mentions_json["mentions"] = sorted(mentions_json["mentions"], key=lambda x: x["receivedMentions"], reverse=True)
-
-    with open("mención.json", "w", encoding="utf-8") as json_file:
-        json.dump(mentions_json, json_file, ensure_ascii=False, indent=2)
+    if arg==True:
+        with open("mención.json", "w", encoding="utf-8") as json_file:
+            json.dump(mentions_json, json_file, ensure_ascii=False, indent=2)
 
     return mentions_json
 
@@ -262,7 +264,7 @@ def generate_mentions_graph(mentions_json):
     nx.write_gexf(G, "mención.gexf")
 
 
-def generate_corrtweets_json(retweets_info):
+def generate_corrtweets_json(retweets_info, arg):
     corrtweets_dict = defaultdict(set)
     # Recopilar información sobre quién retuiteó a cada autor
     for author, author_info in retweets_info.items():
@@ -291,9 +293,9 @@ def generate_corrtweets_json(retweets_info):
     corrtweets_list = sorted(corrtweets_list, key=lambda x: x['totalCoretweets'], reverse=True)
 
     corrtweets_json = {'coretweets': corrtweets_list}
-    
-    with open('corrtw.json', 'w', encoding='utf-8') as json_file:
-        json.dump(corrtweets_json, json_file, ensure_ascii=False, indent=2)
+    if arg==True:
+        with open('corrtw.json', 'w', encoding='utf-8') as json_file:
+            json.dump(corrtweets_json, json_file, ensure_ascii=False, indent=2)
 
 
     return corrtweets_json
@@ -315,25 +317,50 @@ def generate_corrtweets_graph(corrtweets_info):
 
     nx.write_gexf(G, "corrtw.gexf")
 
+def delete_files(folder_path):
+    # Iterate through all the files and subdirectories in the given path
+    for root, dirs, files in os.walk(folder_path):
+        for file in files:
+            file_path = os.path.join(root, file)
+
+            # Verificar si el archivo tiene la extensión .json
+            if file.endswith(".json"):
+                try:
+                    # Eliminar el archivo
+                    os.remove(file_path)
+                except Exception as e:
+                    print(f"Error deleting {file_path}: {e}")
+
 
 if __name__ == "__main__":
     start_time = time.time()
-    directory, fecha_inicial, fecha_final, hashtags_file = parse_args()
-    if fecha_inicial==None and fecha_final==None:
-         retweets_info, mentions_info, json_files = decompress_and_create_json_files_directory(directory, hashtags_file)
-    else:
-         retweets_info, mentions_info, json_files = decompress_and_create_json_files(directory, fecha_inicial, fecha_final, hashtags_file)
+    directory, fecha_inicial, fecha_final, hashtags_file, args = parse_args()
+    retweets_info, mentions_info = decompress_and_create_json_files(directory,hashtags_file, fecha_inicial, fecha_final)
 
     tweets=retweets_info
-    rt_json= generate_retweets_json(retweets_info)
-    generate_retweets_graph(rt_json)
-    mentions_json= generate_mentions_json(mentions_info)
-    generate_mentions_graph(mentions_json)
-    corrtweets_json = generate_corrtweets_json(retweets_info)
-    generate_corrtweets_graph(corrtweets_json)
 
-    for json_file in json_files:
-        os.remove(json_file.with_suffix('.json'))
+    if args.generate_retweet_graph:
+        rt_json = generate_retweets_json(retweets_info, args.generate_retweet_json)
+        generate_retweets_graph(rt_json)
+
+    if args.generate_mentions_graph:
+        mentions_json = generate_mentions_json(mentions_info, args.generate_mentions_json)
+        generate_mentions_graph(mentions_json)
+
+    if args.generate_corretweet_graph:
+        corrtweets_json = generate_corrtweets_json(retweets_info, args.generate_corretweet_json)
+        generate_corrtweets_graph(corrtweets_json)
+
+    if args.generate_retweet_json:
+        generate_retweets_json(retweets_info, args.generate_retweet_json)
+
+    if args.generate_mentions_json:
+        generate_mentions_json(mentions_info, args.generate_mentions_json)
+
+    if args.generate_corretweet_json:
+        generate_corrtweets_json(retweets_info, args.generate_corretweet_json)
+
+    delete_files(directory)
     end_time = time.time()
     total_time = end_time - start_time
     print(f"Proceso completado. Tiempo total de ejecución: {total_time} segundos.")
